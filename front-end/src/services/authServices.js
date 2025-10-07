d7b47f910a38a91b2e8aae26086f8d9344c3252c
mohamed amine <aissaouimohamedamine62@gmail.com>
2025-10-07 14:57:39 +0200

    frontend amélioré7

--- front-end/src/services/authServices.js
+++ front-end/src/services/authServices.js
@@ -3,11 +3,11 @@ import axios from "axios";
 import API_URL from '../config/api';
 
 export const register = async (user) => {
-  const response = await axios.post(`${API_URL}/auth/register`, user);
+  const response = await axios.post(`${API_URL}/api/auth/register`, user);
   return response.data;
 };
 
 export const login = async (credentials) => {
-  const response = await axios.post(`${API_URL}/auth/login`, credentials);
+  const response = await axios.post(`${API_URL}/api/auth/login`, credentials);
   return response.data;
 };
