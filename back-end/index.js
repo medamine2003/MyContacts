b60853f58a5212ce07e1d90fe89d9ec13d06fd34
mohamed amine <aissaouimohamedamine62@gmail.com>
2025-10-07 14:43:01 +0200

    frontend amélioré7

--- back-end/index.js
+++ back-end/index.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://my-contacts-omega-one.vercel.app/login", 
+  "https://my-contacts-inky.vercel.app", 
   process.env.FRONTEND_URL, 
 ];
 
